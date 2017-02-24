65148b487cf9dbbe5fa9ce385e585e315e48c911
Tom Victor <vjtomvictor@icloud.com>
2017-02-24 22:09:33 +0530

    Model update

--- django_cbv/mainapp/models.py
+++ django_cbv/mainapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30,default="Your name")
+    job = models.CharField(max_length=30, default="your Job")
