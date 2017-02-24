92964f648de07cfbd89c0f7cd52190d24fa80cf2
Tom Victor <vjtomvictor@icloud.com>
2017-02-24 22:52:25 +0530

    Detail View working

--- django_cbv/mainapp/urls.py
+++ django_cbv/mainapp/urls.py
@@ -4,4 +4,5 @@ from mainapp import views
 urlpatterns = [
     url(r'^$',views.home_page.as_view(), name='home'),
     url(r'^list$',views.profile_list_view.as_view(), name='list'),
+    url(r'^(?P<pk>[-\w]+)/$',views.profile_detail_view.as_view(), name='detail'),
 ]
--- django_cbv/mainapp/views.py
+++ django_cbv/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from mainapp.models import Profile
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
@@ -14,3 +15,9 @@ class profile_list_view(ListView):
     model = Profile
     template_name = "list.html"
     context_object_name = "object_list"
+
+
+class profile_detail_view(DetailView):
+    model = Profile
+    context_object_name = "object"
+    template_name = "detail.html"
