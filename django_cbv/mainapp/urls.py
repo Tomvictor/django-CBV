f61c0116578af67fc12ac39c1828bfd6fefd97cc
Tom Victor <vjtomvictor@icloud.com>
2017-02-24 22:29:39 +0530

    Template view working

--- django_cbv/mainapp/urls.py
+++ django_cbv/mainapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url,include
 from mainapp import views
 
 urlpatterns = [
-    url(r'^$',views.home, name='home'),
+    url(r'^$',views.home_page.as_view(), name='home'),
 ]
--- django_cbv/mainapp/views.py
+++ django_cbv/mainapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from mainapp.models import Profile
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
 def home(request):
     return render(request,'home.html',{})
+
+class home_page(TemplateView):
+    template_name = "home.html"
