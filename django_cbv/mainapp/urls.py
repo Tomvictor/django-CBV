34268827d1842d17ae13cd773c9ecbef1f4ed1b2
Tom Victor <vjtomvictor@icloud.com>
2017-02-24 22:43:41 +0530

    List view working

--- django_cbv/mainapp/urls.py
+++ django_cbv/mainapp/urls.py
@@ -3,4 +3,5 @@ from mainapp import views
 
 urlpatterns = [
     url(r'^$',views.home_page.as_view(), name='home'),
+    url(r'^list$',views.profile_list_view.as_view(), name='list'),
 ]
--- django_cbv/mainapp/views.py
+++ django_cbv/mainapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from mainapp.models import Profile
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 
 # Create your views here.
 
 class home_page(TemplateView):
     template_name = "home.html"
+
+
+class profile_list_view(ListView):
+    model = Profile
+    template_name = "list.html"
+    context_object_name = "object_list"
