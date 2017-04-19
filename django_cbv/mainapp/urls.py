8f70eda4a3dad3e651dc38cb9ddd630d7a6bf084
Tom Victor <vjtomvictor@icloud.com>
2017-04-19 23:37:43 +0530

    django template url added

--- django_cbv/mainapp/urls.py
+++ django_cbv/mainapp/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url,include
 from mainapp import views
 
 urlpatterns = [
-    url(r'^$',views.home_page.as_view(), name='home'),
-    url(r'^list$',views.profile_list_view.as_view(), name='list'),
+    url(r'^$',views.profile_list_view.as_view(), name='list'),
     url(r'^(?P<pk>[-\w]+)/$',views.profile_detail_view.as_view(), name='detail'),
     url(r'^(?P<pk>[-\w]+)/$',views.ProfileUpdate.as_view(), name='edit'),
     url(r'^create$', views.profile_create_view.as_view(), name='create'),
