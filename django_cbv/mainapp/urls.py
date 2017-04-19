5f5823353eb60f74f9759bd1f822c55287bcc5d9
Tom Victor <vjtomvictor@icloud.com>
2017-04-19 23:38:57 +0530

    Update view working fine

--- django_cbv/mainapp/urls.py
+++ django_cbv/mainapp/urls.py
@@ -4,6 +4,6 @@ from mainapp import views
 urlpatterns = [
     url(r'^$',views.profile_list_view.as_view(), name='list'),
     url(r'^(?P<pk>[-\w]+)/$',views.profile_detail_view.as_view(), name='detail'),
-    url(r'^(?P<pk>[-\w]+)/$',views.ProfileUpdate.as_view(), name='edit'),
+    url(r'^(?P<pk>[-\w]+)/edit$',views.ProfileUpdate.as_view(), name='edit'),
     url(r'^create$', views.profile_create_view.as_view(), name='create'),
 ]
